3027bc94b348adb9112436a060d9f9c8c6359e1b
Michael Pinna <mikepinna3@gmail.com>
2021-10-08 18:41:30 -0400

    Application works

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -8,7 +8,7 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -38,7 +38,7 @@ def register():
     return render_template('auth/register.html')
 
 
-@bp.route('/login', methods=['GET', 'POST'])
+@bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
